bbfe77dfed09f8fb13dc8fef34308b09ce434f6d
Reinaldo Chaves <reichaves@gmail.com>
2020-03-26 19:20:47 -0300

    Add files via upload

--- covid19/covid19_portalms.py
+++ covid19/covid19_portalms.py
@@ -11,8 +11,6 @@
 import requests
 import pandas as pd
 
-
-# CAPTURA CASOS POR REGIOES
 headers = {
     'authority': 'xx9p7hp1p7.execute-api.us-east-1.amazonaws.com',
     'accept': 'application/json, text/plain, */*',
@@ -26,6 +24,8 @@ headers = {
     'accept-language': 'pt-BR,pt;q=0.9,en-US;q=0.8,en;q=0.7,es;q=0.6,it;q=0.5',
 }
 
+
+# CAPTURA CASOS POR REGIOES
 response = requests.get('https://xx9p7hp1p7.execute-api.us-east-1.amazonaws.com/prod/PortalRegiao', headers=headers)
 
 regioes = response.json()
@@ -50,19 +50,6 @@ df_regioes.to_csv('casos_regioes.csv',index=False)
 
 
 # CAPTURA ACUMULO
-headers = {
-    'authority': 'xx9p7hp1p7.execute-api.us-east-1.amazonaws.com',
-    'accept': 'application/json, text/plain, */*',
-    'sec-fetch-dest': 'empty',
-    'x-parse-application-id': 'unAFkcaNDeXajurGB7LChj8SgQYS2ptm',
-    'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.149 Safari/537.36',
-    'origin': 'https://covid.saude.gov.br',
-    'sec-fetch-site': 'cross-site',
-    'sec-fetch-mode': 'cors',
-    'referer': 'https://covid.saude.gov.br/',
-    'accept-language': 'pt-BR,pt;q=0.9,en-US;q=0.8,en;q=0.7,es;q=0.6,it;q=0.5',
-}
-
 response = requests.get('https://xx9p7hp1p7.execute-api.us-east-1.amazonaws.com/prod/PortalAcumulo', headers=headers)
 
 acumulo = response.json()
@@ -84,3 +71,25 @@ df_acumulado = pd.DataFrame(conteudo)
 df_acumulado.to_csv('casos_acumulados.csv',index=False)
 
 
+# CAPTURA POR ESTADOS
+response = requests.get('https://xx9p7hp1p7.execute-api.us-east-1.amazonaws.com/prod/PortalMapa', headers=headers)
+
+estados = response.json()
+
+conteudo = []
+
+for pagina in estados["results"]:
+    dicionario = {
+                "objectId": pagina.get('objectId'), 
+                "nome": pagina.get('nome'), 
+                "qtd_confirmado": pagina.get('qtd_confirmado'), 
+                "latitude": pagina.get('latitude'), 
+                "longitude": pagina.get('longitude'), 
+                "createdAt": pagina.get('createdAt'), 
+                "updatedAt": pagina.get('updatedAt')
+                }
+
+    conteudo.append(dicionario)
+
+df_estados = pd.DataFrame(conteudo)
+df_estados.to_csv('casos_estados.csv',index=False)
